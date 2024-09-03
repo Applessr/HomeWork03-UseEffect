57302e7dfad7594e57c399e565203bfedc69036b
Applesst <Sasita_ssr@kkumail.com>
2024-09-03 09:33:09 +0700

    REfrator: Product-Search

--- src/components/ProduceList.jsx
+++ src/components/ProduceList.jsx
@@ -2,12 +2,24 @@ import React, { useState, useEffect } from 'react';
 
 export default function ProduceList({ search }) {
   const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchData = async () => {
-    const resp = await fetch(`https://dummyjson.com/products/search?q=${search}`);
-    const result = await resp.json();
-    setData(result.products);
-   
+    setLoading(true);
+    setError(null);
+    try {
+      const resp = await fetch(`https://dummyjson.com/products/search?q=${search}`);
+      if (!resp.ok) {
+        throw new Error('Failed to fetch');
+      }
+      const result = await resp.json();
+      setData(result.products);
+    } catch (err) {
+      setError('Failed to fetch data');
+    } finally {
+      setLoading(false);
+    }
   };
 
   useEffect(() => {
@@ -16,19 +28,34 @@ export default function ProduceList({ search }) {
     }
   }, [search]);
 
+  if (loading) {
+    return <div className='text-white'>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
-<div>
-  <ul className='flex flex-col mx-10'>
-    {data.map((product) => (
-      <li className='p-5 text-neutral-content border rounded-md m-2 hover:bg-base-300 hover:text-white' 
-      key={product.id}>
-        <p className='text-[20px] font-semibold py-1'>{product.title} </p>
-        <p  className='text-[18px] '>{product.category} </p>
-        <p className='py-1'>${product.price}</p> 
-      </li>
-    ))}
-  </ul>
-</div>
+    <div>
+      <ul className='flex flex-col mx-10'>
+        {data.length > 0 ? (
+          data.map((product) => (
+            <li
+              className='p-5 text-neutral-content border rounded-md m-2 hover:bg-base-300 hover:text-white'
+              key={product.id}
+            >
+              <div>
+                <p className='text-[20px] font-semibold py-1'>{product.title}</p>
+                <p className='text-[18px]'>{product.category}</p>
+                <p className='py-1'>${product.price}</p>
+              </div>
+            </li>
+          ))
+        ) : (
+          <p className='text-white'>Product Not Found</p>
+        )}
+      </ul>
+    </div>
   );
 }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,25 +1,20 @@
 import React, { useState, useEffect } from 'react';
 
-function debounce(func, delay) {
-  let timer;
-  return function(...args) {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func.apply(this, args);
-    }, delay);
-  };
-}
-
-export default function SearchBar({ setSearch}) {
+export default function SearchBar({ setSearch }) {
   const [input, setInput] = useState('');
 
-  const debouncedSearch = debounce((value) => {
-    setSearch(value);
-  }, 500);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSearch(input);
+    }, 1000);
+
+    return () => {
+      clearTimeout(timer); 
+    };
+  }, [input]); 
 
   const handleInputChange = (e) => {
     setInput(e.target.value);
-    debouncedSearch(e.target.value);  
   };
 
   return (
